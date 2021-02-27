f8a37085382f7982778935d85a4efe9410bd9219
Filip Oščádal <f@mxd.cz>
2021-02-27 01:13:08 +0100

    web sync

--- app/NewzPresenter.php
+++ app/NewzPresenter.php
@@ -29,19 +29,9 @@ class NewzPresenter extends APresenter
         $view = $this->getView();
         $this->checkRateLimit()->setHeaderHtml()->dataExpander($data); // data = Model
 
-        // advanced caching
-        $use_cache = (DEBUG === true) ? false : $data["use_cache"] ?? false;
-        $cache_key = strtolower(join("_", [$data["host"], $data["request_path"]])) . "_htmlpage";
-        if ($use_cache && $output = Cache::read($cache_key, "page")) {
-            header("X-Cached: true");
-            return $this->setData("output", $output);
-        }
-
         // output
         $output = $this->setData($data)->renderHTML($presenter[$view]["template"]); // render
         StringFilters::trim_html_comment($output); // fix content
-        Cache::write($cache_key, $output, "page"); // save cache
-        header("X-Cached: false");
         return $this->setData("output", $output); // save model
     }
 }
